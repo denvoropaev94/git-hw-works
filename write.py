b87d840859b5b1d5abaa8f5111f0f74ce43d6340
«Denis» <denvoropaev7@gmail.com>
2023-08-03 10:27:43 +0300

    func write_csv del

--- write.py
+++ write.py
@@ -2,11 +2,11 @@ import csv
 import json
 
 
-def write_csv(note_data):
-    with open('database.csv', 'w', encoding="utf-8") as file:
-        writer = csv.writer(file)
-        for note in note_data:
-            writer.writerow(note.values())
+# def write_csv(note_data):
+#     with open('database.csv', 'w', encoding="utf-8") as file:
+#         writer = csv.writer(file)
+#         for note in note_data:
+#             writer.writerow(note.values())
 
 def write_json(note_data):
     with open('database02.json', 'w') as outfile:
